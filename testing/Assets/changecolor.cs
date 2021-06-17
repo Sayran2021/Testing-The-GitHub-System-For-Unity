d64be1e7acc344cf6e745addd3eedb6bcd22de66
Margulan <turganbekmargulan@mail.ru>
2021-06-17 21:22:39 +0600

    first commit
    ----dfksaefeopafdsaf

---
+++ testing/Assets/changecolor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changecolor : MonoBehaviour
+{
+    public GameObject circle;
+
+    private SpriteRenderer thething;
+
+    private void Start() {
+       thething = circle.GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeColor()
+    {
+        thething.color = Color.black;
+    }
+}
